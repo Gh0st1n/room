1b0abd8c7d41c2ad098bbede73ea7c105a8d20df
ghostin <lsway1998@gmail.com>
2022-12-16 10:16:26 +0800

    feat: get data from github api

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -7,6 +7,68 @@ import path from 'path';
 
 const privateKey = fs.readFileSync(path.resolve(process.cwd(),`${process.env.PEM_FILENAME}`), { encoding: "utf8" });
 
+const GET_DISSCUSSIONS_QUERY =()=> `query discussions {
+    repository(owner: "Gh0st1n", name: "tbbr.io") {
+      discussions(
+        last: 10
+        # before: "Y3Vyc29yOnYyOpK5MjAyMi0xMi0xNVQxNzoyNzoxNCswODowMM4ARzKS"
+        orderBy: {field: CREATED_AT, direction: DESC}
+      ) {
+        nodes {
+          ...discussionFields
+        }
+        edges {
+          cursor
+        }
+        totalCount
+      }
+    }
+  }
+  
+  fragment discussionFields on Discussion {
+    id
+    url
+    locked
+    number
+    title
+    resourcePath
+    author {
+      login
+      url
+      ... on User {
+        id
+        email
+        avatarUrl
+        name
+        login
+        websiteUrl
+      }
+      resourcePath
+      avatarUrl
+    }
+    body
+    bodyHTML
+    bodyText
+    category {
+      name
+      id
+      slug
+      emoji
+      emojiHTML
+      description
+    }
+    comments(last: 10) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+    publishedAt
+    updatedAt
+  }
+`
+
 export default async function handler(req, res) {
     const octokit = new Octokit({
         authStrategy: createAppAuth,
@@ -17,9 +79,12 @@ export default async function handler(req, res) {
         }
     });
     
-    const {data} = await octokit.rest.apps.getAuthenticated();
+    // authenticate as an installation
+    await octokit.rest.apps.getAuthenticated();
 
-    res.status(200).json(data)
+    const discussionData = await octokit.graphql(GET_DISSCUSSIONS_QUERY())
+
+    res.status(200).json(discussionData.repository)
 }
 
 
